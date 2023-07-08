2dc156d46cec160158ad21402efbc8d8e2583772
AnshulChamoli03 <anshulchamoli03@gmail.com>
2023-07-08 21:54:39 +0530

    final designed done

---
+++ src/components/Image.js
@@ -0,0 +1,10 @@
+import "./Image.css";
+import React from 'react';
+
+export default function Image() {
+  return (
+    <div className='imgcontainer'>
+      <img src="https://static-00.iconduck.com/assets.00/web-developer-illustration-503x512-mega3xxj.png" alt="1" size="cover"></img>
+    </div>
+  )
+}
