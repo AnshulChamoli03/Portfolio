42d84d45526ee74b512ea55145cf62825b602ac8
AnshulChamoli03 <anshulchamoli03@gmail.com>
2024-09-19 21:19:59 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.1c29755bc3bbab6968f4ac3000a66895.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3363ffab9d97a04e0e2e009c5a6c8c34",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a2aadf4731520d7b0ad2",
+    "url": "/Portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "555cab87a014b157b429",
+    "url": "/Portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "a2aadf4731520d7b0ad2",
+    "url": "/Portfolio/static/js/2.97954de5.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio/static/js/2.97954de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "555cab87a014b157b429",
+    "url": "/Portfolio/static/js/main.3b1640e4.chunk.js"
+  },
+  {
+    "revision": "c125469937b460e6de34",
+    "url": "/Portfolio/static/js/runtime-main.3379861c.js"
+  }
+]);
